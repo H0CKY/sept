9209ffc6412fac32377cb3f6224f7df2d80f88f7
H0CKY <112852916+H0CKY@users.noreply.github.com>
2022-09-21 13:15:02 +0200

    2.0

---
+++ Test2.0.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adjon meg egy egész számot!");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adjon meg még egy egész számot!");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadLine();
+        }
+    }
+}
