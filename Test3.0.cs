f7986f1eb09fffe3496f4d5023c103c5af08ecb9
H0CKY <112852916+H0CKY@users.noreply.github.com>
2022-09-21 13:15:15 +0200

    3.0

---
+++ Test3.0.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adjon meg egy egész számot!");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adjon meg még egy egész számot!");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("A nagyobb szam:{0}", a);
+            }
+            else { Console.WriteLine("A nagyobb szam:{0}", b); }
+
+            Console.ReadLine();
+        }
+    }
+}
